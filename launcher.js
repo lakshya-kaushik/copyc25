234edd0d483aa0f9c34c47b79a82f77c9c796d4d
lakshya-kaushik <68064181+lakshya-kaushik@users.noreply.github.com>
2020-08-24 23:09:06 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,29 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 30
+        }
+        this.pointB = pointB;
+        this.launch = Constraint.create(options);
+        World.add(world, this.launch);
+    }
+
+    fly(){
+        this.launch.bodyA = null;
+    }
+
+    display(){
+    
+         if (this.launch.bodyA){
+            var pointA = this.launch.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        
+         }
+    }
+    
+}
